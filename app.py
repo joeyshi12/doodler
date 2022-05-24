a202f10107a1065e601550d7880291c576c28180
Joey <joeyshi12@gmail.com>
2022-05-24 01:03:43 -0700

    Clean up

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, render_template, request, Response
+import io
+import tempfile
+from flask import Flask, render_template, request, Response, jsonify
 from flask_cors import CORS
 from PIL import Image, ImageDraw, ImageFont
-import tempfile
-import io
+
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
 CORS(app)
@@ -16,27 +18,36 @@ def index():
 
 @app.route("/scribble", methods=["POST"])
 def scribble():
-    file = request.files["file"]
-    temp = tempfile.mktemp()
-    os.mkdir(temp)
-    file_path = os.path.join(temp, file.name)
+    if "image" not in request.files:
+        return jsonify({"message": "image not found in files"}), 500
+    try:
+        buffer = __process_image(request.files["image"])
+        return Response(buffer.getvalue(), mimetype="image/png")
+    except:
+        return jsonify({"message": "error in processing image"}), 500
 
+
+def __process_image(file: FileStorage) -> io.BytesIO:
+    tempdir_name = tempfile.mktemp()
+    os.mkdir(tempdir_name)
+    file_path = os.path.join(tempdir_name, file.name)
     file.stream.seek(0)
     file.save(file_path)
     file.close()
 
-    img = Image.open(file_path)
-    draw = ImageDraw.Draw(img)
+    image = Image.open(file_path)
+    draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(r"static/fonts/sauce_code_pro.ttf", 50)
     draw.text((5, 5), "Peter Sux", font=font, align="left", fill="black")
-    buf = io.BytesIO()
-    img.save(buf, format="PNG")
+
+    buffer = io.BytesIO()
+    image.save(buffer, format="PNG")
 
     if os.path.exists(file_path):
         os.remove(file_path)
 
-    return Response(buf.getvalue(), mimetype="image/png")
+    return buffer
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
